fa1dd10ee93badbb116f4a1f300d779a3030e31c
jasonari <jason.ari@outlook.com>
2025-04-23 21:41:26 +0800

    chore: update not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,20 @@
+'use client'
 import type { NextPage } from 'next'
 
 const NotFound: NextPage = () => {
-  return <div>NotFound</div>
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="p-4 text-4xl font-bold">404 Not Found</h1>
+      <button
+        className="btn btn-neutral"
+        onClick={() => {
+          window.location.replace('/')
+        }}
+      >
+        Back to Home
+      </button>
+    </div>
+  )
 }
 
 export default NotFound
