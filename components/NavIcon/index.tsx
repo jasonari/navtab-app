c21725ff745c3af53928bbc219ddb5546c6922a1
jasonari <jason.ari@outlook.com>
2025-05-13 01:22:08 +0800

    pref: replace the img element with Image component

--- components/NavIcon/index.tsx
+++ components/NavIcon/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import type { Bookmark } from '@/types'
 
 interface NavIconProps {
@@ -13,10 +14,11 @@ const NavIcon: NextPage<NavIconProps> = ({ icon }) => {
       rel="noopener noreferrer"
       className="flex-center mr-2 ml-2 flex size-[60px] rounded-xl bg-[#fff]"
     >
-      <img
+      <Image
         src={icon.imgUrl}
         alt={icon.title}
-        className={`size-[${icon.size}px]`}
+        width={icon.size}
+        height={icon.size}
       />
     </a>
   )
