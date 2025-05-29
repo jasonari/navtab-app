fce1cc94cfbb1b9909ff7424d39105dabb9772b1
jasonari <jason.ari@outlook.com>
2025-05-30 02:11:01 +0800

    fix: type error in user button

--- components/Header/user-button.tsx
+++ components/Header/user-button.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation'
 import { signOutUser } from '@/lib/actions/user.action'
 
-const UserButton = ({ user }: { user: { name: string } }) => {
+const UserButton = ({ user }: { user: any }) => {
   const router = useRouter()
 
   if (!user) {
