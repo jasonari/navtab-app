08198c575413c8f27dc743d26f367f3a95a6ab77
jasonari <jason.ari@outlook.com>
2025-07-18 00:06:22 +0800

    chore: add husky, commitlint and lint-staged

---
+++ commitlint.config.mjs
@@ -0,0 +1,43 @@
+// commitlint.config.mjs
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // header
+    'header-max-length': [2, 'always', 72],
+
+    // subject
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+
+    // body
+    'body-leading-blank': [2, 'always'],
+
+    // footer
+    'footer-leading-blank': [2, 'always'],
+
+    // type
+    'type-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'build',
+        'ci'
+      ]
+    ]
+  }
+}
